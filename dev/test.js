36a980f00a8e2d01e3c33744b62f2a60de18e506
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2021-09-12 14:12:09 +0500

    added express server

--- dev/test.js
+++ dev/test.js
@@ -2,4 +2,20 @@ const BLockchain = require("./blockchain");
 
 const bitcoin = new BLockchain();
 
-console.log(bitcoin);
+const prevBlockHash = "bnduyedggtd6dg";
+const currentBlockData = [
+    {
+        amount: 100,
+        sender: "bcbcyc6437463763",
+        reciepent: "44ur7fhyfftgfgf"
+    
+    },
+    {
+        amount: 167,
+        sender: "cbchcbrcgryrrr",
+        reciepent: "rhcruhrygcrygcr"
+    
+    }
+];
+
+console.log(bitcoin.proofOfWork(prevBlockHash, currentBlockData));
